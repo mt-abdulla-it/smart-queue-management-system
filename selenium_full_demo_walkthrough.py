cabb10f46bc9486ec5a9d4bcac53c09bde885977
mt-abdulla-it <mt.abdulla.it@gmail.com>
2026-07-23 10:49:26 +0530

    feat: configure chrome options to hide automation detection flags

--- selenium_full_demo_walkthrough.py
+++ selenium_full_demo_walkthrough.py
@@ -22,6 +22,9 @@ def run_full_sqms_demo():
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option("useAutomationExtension", False)
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.implicitly_wait(5)
